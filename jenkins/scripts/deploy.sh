04ceb2807357b0307b209fcab6caa52463ca2daa
xYogiiBear <1601923D@student.tp.edu.sg>
2023-10-31 17:56:49 +0800

    Added change to script

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+#docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v https://github.com/gregerchen99/Jenkins-Automated-Testing.git
 sleep 1
 set +x
 
