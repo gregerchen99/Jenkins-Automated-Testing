fc43390811fcea1c208ab2e76a47620768c3bdd1
xYogiiBear <1601923D@student.tp.edu.sg>
2023-11-01 23:04:35 +0800

    fdfdf

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -x
 #docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
-docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/Jenkins-Automated-Testing//src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/Jenkins-Automated-Testing/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
